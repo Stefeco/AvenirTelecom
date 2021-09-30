e58c9a775a948c94b7df23a758a4648caec424c2
Stefeco <Namgarn@gmail.com>
2021-09-30 16:43:05 +0200

    ..

--- force-app/main/default/lwc/homePage/homePage.js
+++ force-app/main/default/lwc/homePage/homePage.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
+import AvTelecom_logo from '@salesforce/resourceUrl/AvTelecom_logo';
 
-export default class HomePage extends LightningElement {}
+export default class HomePage extends LightningElement {
+
+    //déclaration de AvTelecomImg pour insérer dans page d'accueil
+    AvTelecomImg = AvTelecom_logo;
+}
